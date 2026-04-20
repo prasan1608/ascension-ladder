be19c22f45edcaa6c49af32ff393a5919095e321
prasan1608 <prasanjit.flyshop@gmail.com>
2026-04-20 23:07:28 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "ascension-ladder-v1";
+const APP_ASSETS = [
+  "./",
+  "./Level%20Up%20Life.html",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./icon-apple.svg"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request).then(function (response) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match("./Level%20Up%20Life.html");
+      });
+    })
+  );
+});
